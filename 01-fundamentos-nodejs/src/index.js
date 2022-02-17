9ab5fd14408664705f9efb91c44606717c30a2ab
Flávia Rocha <flaviaccruzr@gmail.com>
2022-02-17 15:02:55 -0300

    http verbs fundamentals

--- 01-fundamentos-nodejs/src/index.js
+++ 01-fundamentos-nodejs/src/index.js
@@ -2,8 +2,48 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello World!");
+app.use(express.json());
+
+/**
+ * GET => Get info on server
+ * POST => Insert info on server
+ * PUT => Change info on server
+ * PATCH => Change info partilly
+ * DELETE => Delete info on server
+ */
+
+/**
+ * Params types
+ *
+ * Route Params => Identify resource to edit or delete or search (url/:id)
+ * Query Params => Pagination / Filter
+ * Body Params => Objects to insert or change a resource (JSON)
+ */
+
+app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333);
