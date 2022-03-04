f20b3450b2d01c20fad368427a8ec4272a92c117
Flávia Rocha <flaviaccruzr@gmail.com>
2022-03-04 15:10:30 -0300

    chore: create categories repository

---
+++ 02-rentalx/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../model/Category";
+
+/**
+ * DTO => Data Transfer Object
+ * Criar um objeto que é responsável pela transferência de dados de uma classe à outra
+ */
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
